2393197e57e5ded1146fe9dcf5b51386e5ef7504
Mikhail <mikle0240@gmail.com>
2023-09-14 14:56:10 +0300

    Initial commit

--- src/test/java/ru/netology/javaqa/hwCycles/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa/hwCycles/services/VacationServiceTest.java
@@ -1,11 +1,12 @@
 package ru.netology.javaqa.hwCycles.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
     @ParameterizedTest
-    @CsvFileSource (files = "src/test/resources/vacation.csv")
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
     public void testVacationService(int expected, int income, int expenses, int threshold) {
 
         VacationService service = new VacationService();
